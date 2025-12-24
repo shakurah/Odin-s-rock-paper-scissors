c4629144017fa781b266c703ab78cf8e747f1af0
shakurah <emmanueladjerry@gmail.com>
2025-12-24 06:49:11 +0100

    Add playRound function for rock, paper, scissors

---
+++ script.js
@@ -0,0 +1,57 @@
+humanScore = 0
+computerScore = 0
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    index =  Math.floor(Math.random() * (choices.length))
+    return choices[index]
+
+}
+
+function getHumanChoice() {
+    let user_choice = prompt("rock, paper, scissors..?")
+    user_choice = user_choice.toLowerCase()
+    return user_choice
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice){
+        console.log("Tie! You both choose " + computerChoice)
+    }
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("Human won! " + humanChoice + " beats " + computerChoice )
+        humanScore +=1
+        console.log("Human Score is " + humanScore)
+    }
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("Human won! " + humanChoice + " beats " + computerChoice )
+        humanScore +=1
+        console.log("Human Score is " + humanScore)
+    }
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("Human won! " + humanChoice + " beats " + computerChoice )
+        humanScore +=1
+        console.log("Human Score is " + humanScore)
+    }
+    if (computerChoice == "rock" && humanChoice == "scissors") {
+        console.log("Computer won!"  + computerChoice + " beats " + humanChoice)
+        computerScore +=1
+        console.log("Computer Score is " + computerScore)
+    }
+    if (computerChoice == "scissors" && humanChoice == "paper") {
+        console.log("Computer won! " + computerChoice + " beats " + humanChoice)
+        computerScore +=1
+        console.log("Computer Score is " + computerScore)
+    }
+    if (computerChoice == "paper" && humanChoice == "rock") {
+        console.log("Computer won! " + computerChoice + " beats " + humanChoice )
+        computerScore +=1
+        console.log("Computer Score is " + computerScore)
+    }
+    
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
